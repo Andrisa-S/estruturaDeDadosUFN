83a135a6ab5ecb1f73c7e74813b01a89c53a9137
Andrisa-S <148649363+Andrisa-S@users.noreply.github.com>
2025-03-07 09:13:16 -0300

    Add files via upload

---
+++ cSharp/Lista/Program.cs
@@ -0,0 +1,33 @@
+﻿//Criação de lista
+List<int> lista = new List<int>();
+
+//Randomização
+Random gerador = new Random();
+
+int qtd = gerador.Next(10, 20);
+for (int i = 0; i < qtd; i++)
+{
+    lista.Add(gerador.Next(1000));
+}
+
+//Ordenar
+lista.Sort();
+
+//Exibir por índice
+//for (int i = 0; i < lista.Count; i++)
+//{
+//    Console.WriteLine(lista[i]);
+//}
+
+//Exibir por objeto
+/* foreach (var i  in lista)
+{
+    Console.WriteLine(i);
+} */
+
+//Exibir por iteração
+for (var i = lista.GetEnumerator(); i.MoveNext(); )
+{
+    int numero = i.Current;
+    Console.WriteLine(numero);
+}
---
+++ cSharp/ListaNomes/Program.cs
@@ -0,0 +1,35 @@
+﻿//Criação da lista
+List<String> listaNomes = new List<string>();
+string opcao;
+string nome;
+
+do
+{
+    Console.WriteLine("\nDigite um nome completo: ");
+    nome = Console.ReadLine();
+    nome = nome.ToUpper();
+
+    //Adicionar nomes não cadastrados
+    if (!listaNomes.Contains(nome))
+    {
+        listaNomes.Add(nome);
+    } else
+    {
+        Console.WriteLine("Nome já cadastrado!");
+    }
+
+    Console.WriteLine("\n1 - Continua;\n2 - Finaliza:");
+    opcao = Console.ReadLine();
+} while (opcao != "2");
+
+//Ordenar
+listaNomes.Sort();
+
+//Exibir quantia de nomes
+Console.WriteLine("A lista contém: "+ listaNomes.Count + " nomes\n");
+
+//Exibir lista ordenada
+foreach (var i in listaNomes)
+{
+    Console.WriteLine(i);
+}
